afb1df6328a596469528e17d431836175ce7a7bf
xiaozhen <xiaozhenoh@163.com>
2023-04-12 08:04:52 +0800

    项目初始化

---
+++ web/src/utils/request.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import type { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig, AxiosError } from 'axios';
+import { HttpStatus, HttpStatusType } from './http'
+import { ElMessage } from 'element-plus';
+
+const instance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8'
+  }
+});
+
+instance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  console.log(config.url, import.meta.env.VITE_API_BASE_URL);
+  return config
+})
+
+instance.interceptors.response.use((res: AxiosResponse<any>) => {
+  const { status, data } = res
+  if (status === 200) {
+    const { code, data: response, msg } = data
+    if (code === 0) {
+      return response
+    } else {
+      ElMessage.error(msg)
+      return Promise.reject(data)
+    }
+  }
+  return res
+}, (error: AxiosError) => {
+  const { status } = error.response || {}
+  if (status && status !== 200) {
+    const msg = `errMsg${status}` as HttpStatusType
+    ElMessage.error(HttpStatus[msg])
+  }
+})
+
+export default instance
+
